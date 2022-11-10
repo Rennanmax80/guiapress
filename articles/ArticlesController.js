db50cc8119c06884570d6d71c6abdfc2192f5a11
Rennan <rennanmax80@gmail.com>
2022-11-09 23:15:23 -0300

    instalação tiny / criação artigo new / edição categories

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,13 +1,18 @@
 const express = require("express")
 const router = express.Router()
 
+const Category = require("../categories/Category.model")
+
 router.get("/articles", (req, res) => {
     res.send("ROTA DE Artigos")
 })
 
 
 router.get("/admin/articles/new", (req, res) => {
-    res.send("ROTA DE ARTIGOS NEW")
+    Category.findAll().then(categories => {
+        
+        res.render("admin/articles/new", {categories: categories})
+    })
 })
 
 module.exports = router;
